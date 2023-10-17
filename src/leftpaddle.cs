8a3de7b1c44f40bc0671a5f117d511e810b39334
Nicholas Lee <nickyjedi@Nicholass-MacBook-Pro-2017.local>
2023-10-16 23:26:46 -0700

    Added the godot project

---
+++ src/leftpaddle.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class leftpaddle : Area2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		ProcessInput();
+	}
+
+	private void ProcessInput() {
+		float input = Input.GetActionStrength()
+	}
+}
