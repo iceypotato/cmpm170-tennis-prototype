4b723b9087db7392afb23073b208219ee3f8a5b1
iceypotato <nickyjedi@gmail.com>
2023-10-19 12:30:43 -0700

    Refactored some code

--- src/LeftPaddleArea.cs
+++ src/LeftPaddleArea.cs
@@ -1,8 +1,9 @@
 using Godot;
 using System;
 
-public partial class RightPaddle : Area2D
+public partial class LeftPaddleArea : Area2D
 {
+	private bool _touchingWall = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,27 +12,29 @@ public partial class RightPaddle : Area2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		ProcessInput();
+		// ProcessInput();
 	}
 
 	private void ProcessInput()
 	{
-		if (Input.GetActionStrength("right paddle move up") > 0)
+		if (_touchingWall) return;
+		if (Input.GetActionStrength("left paddle move up") > 0)
 		{
 			this.Position += new Vector2(0, -2);
 		}
-		if (Input.GetActionStrength("right paddle move down") > 0)
+		if (Input.GetActionStrength("left paddle move down") > 0)
 		{
 			this.Position += new Vector2(0, 2);
 		}
-		if (Input.GetActionStrength("right paddle move left") > 0)
+		if (Input.GetActionStrength("left paddle move left") > 0)
 		{
 			this.Position += new Vector2(-2, 0);
 		}
-		if (Input.GetActionStrength("right paddle move right") > 0)
+		if (Input.GetActionStrength("left paddle move right") > 0)
 		{
 			this.Position += new Vector2(2, 0);
 		}
+		
 	}
 
 	public void OnAreaEntered(Area2D area)
@@ -39,7 +42,7 @@ public partial class RightPaddle : Area2D
 		if (area is Ball ball)
 		{
 			// Assign new direction
-			ball.Direction = new Vector2(-1, ((float)new Random().NextDouble()) * 2 - 1).Normalized();
+			ball.Direction = new Vector2(1, ((float)new Random().NextDouble()) * 2 - 1).Normalized();
 		}
 	}
 }
--- src/RightPaddleArea.cs
+++ src/RightPaddleArea.cs
@@ -1,9 +1,8 @@
 using Godot;
 using System;
 
-public partial class LeftPaddle : Area2D
+public partial class RightPaddleArea : Area2D
 {
-	private bool _touchingWall = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -12,29 +11,27 @@ public partial class LeftPaddle : Area2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		ProcessInput();
+		// ProcessInput();
 	}
 
 	private void ProcessInput()
 	{
-		if (_touchingWall) return;
-		if (Input.GetActionStrength("left paddle move up") > 0)
+		if (Input.GetActionStrength("right paddle move up") > 0)
 		{
 			this.Position += new Vector2(0, -2);
 		}
-		if (Input.GetActionStrength("left paddle move down") > 0)
+		if (Input.GetActionStrength("right paddle move down") > 0)
 		{
 			this.Position += new Vector2(0, 2);
 		}
-		if (Input.GetActionStrength("left paddle move left") > 0)
+		if (Input.GetActionStrength("right paddle move left") > 0)
 		{
 			this.Position += new Vector2(-2, 0);
 		}
-		if (Input.GetActionStrength("left paddle move right") > 0)
+		if (Input.GetActionStrength("right paddle move right") > 0)
 		{
 			this.Position += new Vector2(2, 0);
 		}
-		
 	}
 
 	public void OnAreaEntered(Area2D area)
@@ -42,12 +39,7 @@ public partial class LeftPaddle : Area2D
 		if (area is Ball ball)
 		{
 			// Assign new direction
-			ball.Direction = new Vector2(1, ((float)new Random().NextDouble()) * 2 - 1).Normalized();
-		}
-
-		if (area is Walls wall || area is Boundary boundary)
-		{
-			_touchingWall = true;
+			ball.Direction = new Vector2(-1, ((float)new Random().NextDouble()) * 2 - 1).Normalized();
 		}
 	}
 }
